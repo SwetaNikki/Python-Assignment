c923a4c61c0ffb17230e1a08259b0ebc3d0e306a
SwetaNikki <56434154+SwetaNikki@users.noreply.github.com>
2020-07-27 11:38:36 +0530

    Update VolumneOfSphere.py

--- VolumneOfSphere.py
+++ VolumneOfSphere.py
@@ -1,1 +1,16 @@
+#Write a Python program to find the volume of a sphere with diameter 12 cm.
 
+def VolumeOfSphere(r):
+  PI = 3.142
+  return (4/3)*PI*Cube(r);
+
+def Cube(r):
+  return r*r*r;
+  
+def radius(d):
+    return d/2;
+
+d = 12  
+print("The diameter of Sphere : %.i " % d);
+r = radius(d)
+print("The Volume of Sphere of radius 12 is:  %.6f " % VolumeOfSphere(r));
