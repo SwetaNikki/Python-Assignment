5e3f47ed67acd8ce48f28cb853a68f7af35f99d7
SwetaNikki <56434154+SwetaNikki@users.noreply.github.com>
2020-07-27 12:31:02 +0530

    Update NameInReverse.py

--- NameInReverse.py
+++ NameInReverse.py
@@ -1,1 +1,18 @@
 
+#Write a Python program to accept the user's first and last name and then getting them
+#printed in the the reverse order with a space between first name and last name.
+
+fname = input("Enter first name : ")
+space = " "
+lname = input("Enter last name : ")
+name = fname + space + lname
+print("Your full name is : {0}".format(name))
+
+def ReverseName(n) :
+    rev = ""
+    for i in n:
+        rev = i + rev
+    return rev;
+    
+print("{0} in reverse order : ".format(name),end="") 
+print (ReverseName(fname),ReverseName(lname), sep=' ')
